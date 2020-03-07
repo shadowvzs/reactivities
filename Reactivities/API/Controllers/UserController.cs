f8d6be4e91dc0cff29ba3b74de80eac188340e4c
shadowvzs <shadowvzs@hotmail.com>
2020-03-07 17:30:29 +0200

    step 12: get current user

--- Reactivities/API/Controllers/UserController.cs
+++ Reactivities/API/Controllers/UserController.cs
@@ -7,19 +7,27 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
     public class UserController : BaseController
     {
+        [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<User>> Login(Login.Query query)
         {
             return await Mediator.Send(query);
         }
 
+        [AllowAnonymous]
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register(Register.Command command)
         {
             return await Mediator.Send(command);
         }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<User>> CurrentUser()
+        {
+            return await Mediator.Send(new CurrentUser.Query());
+        }        
     }
 }
