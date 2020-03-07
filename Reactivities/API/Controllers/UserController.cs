0765c13558bf9bc51c2df6371211b8a57b069d33
shadowvzs <shadowvzs@hotmail.com>
2020-03-07 16:09:47 +0200

    step 12: Added register and custom fluent validation

--- Reactivities/API/Controllers/UserController.cs
+++ Reactivities/API/Controllers/UserController.cs
@@ -7,13 +7,19 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class UserController : BaseController
     {
-        [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<User>> Login(Login.Query query)
         {
             return await Mediator.Send(query);
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(Register.Command command)
+        {
+            return await Mediator.Send(command);
         }
     }
 }
