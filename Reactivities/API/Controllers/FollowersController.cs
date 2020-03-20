9e5c67120189f147bca82770e4a9de181f7df88c
shadowvzs <shadowvzs@hotmail.com>
2020-03-20 22:26:21 +0200

    step 20: Added UserFollower table/migration, relationships (followings, followers), Follower handlers (add/delete) and controller

---
+++ Reactivities/API/Controllers/FollowersController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Application.Followers;
+using MediatR;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/profiles")]
+    public class FollowersController : BaseController
+    {
+        [HttpPost("{username}/follow")]
+        public async Task<ActionResult<Unit>> Follow(string username)
+        {
+            return await Mediator.Send(new Add.Command{Username = username});
+        }
+
+        [HttpDelete("{username}/follow")]
+        public async Task<ActionResult<Unit>> Unfollow(string username)
+        {
+            return await Mediator.Send(new Delete.Command{Username = username});
+        }
+    }
+}
---
+++ Reactivities/Application/Followers/Add.cs
@@ -0,0 +1,60 @@
+using MediatR;
+using Domain;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+using Application.Errors;
+using Application.Interfaces;
+
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Followers
+{
+    public class Add
+    {
+        public class Command : IRequest
+        {
+            public string Username { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                _userAccessor = userAccessor;
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+
+                var observer = await _context.Users.SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetCurrentUsername());
+
+                var target = await _context.Users.SingleOrDefaultAsync(x => x.UserName == request.Username);
+                if (target == null)
+                    throw new RestException(HttpStatusCode.NotFound, new { User = "Activity not exist" });
+
+                var following = await _context.Followings.SingleOrDefaultAsync();
+                if (following != null)
+                    throw new RestException(HttpStatusCode.BadRequest, new { User = "You are already following this user" });
+                following = new UserFollowing
+                {
+                    Observer = observer, 
+                    Target = target
+                };
+
+                _context.Followings.Add(following);
+
+                var success = await _context.SaveChangesAsync() > 0;                
+                if (success) return Unit.Value;
+
+                throw new RestException(HttpStatusCode.NotFound, new { following = "Problem saving changes" });
+            }
+        }
+    }
+}
